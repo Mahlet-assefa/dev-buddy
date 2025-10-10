94a33346b0beb8ca040e1f8a6df3cf4ef14454bf
Mahlet-assefa <mahletassefacodes@gmail.com>
2025-10-10 23:16:29 +0300

    added sign up page

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,13 +1,19 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import { z } from "zod"
-import { motion } from "framer-motion"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Eye, EyeOff, Github, Mail } from "lucide-react"
+import { z } from "zod";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Eye, EyeOff, Github, Mail } from "lucide-react";
 
 export default function SignUpPage() {
-    
+  const [name,setname]=useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {},
+  );
 }
